d9d9d7aa1e796f6929b5bb324cbe039544dac3df
sandra <mjfelici@gmail.com>
2022-01-17 09:53:46 -0300

    validar formularios con javascript

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 const msgErro = document.querySelector("#error-msg");
-const input = document.querySelector("#emails").value;
+const input = document.querySelector("#emails");
 const button = document.querySelector(".button")
 const form = document.querySelector('form');
 const msgEmail = document.querySelector(".conteudo-input");
 
 
+
+
 button.addEventListener("click", (ev) => {
 
     const msgPlace = document.querySelector(".conteudo-input").placeholder = "exemplo@email.com";
-    if (input === '') {
+    if (input.value === '') {
         msgErro.style.display = 'flex';
         form.style.border = '0.8px solid red';
-
     }
 
 })
 msgEmail.addEventListener("focus", (ev) => {
     const msgPlace = document.querySelector(".conteudo-input").placeholder = "Your email address...";
     msgErro.style.display = "none"
+    form.style.border = 'none';
+
     console.log({ msgErro })
 
 })
